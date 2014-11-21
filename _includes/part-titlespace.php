575b0ed7d4aeca1b0e017067bad2c2843f763743
Nick Worth <nworth@raprec.com>
2014-11-20 18:59:09 -0700

    setup jekyll structure

---
+++ _includes/part-titlespace.php
@@ -0,0 +1,8 @@
+<div id="header-top" class="clearfix">
+	<div id="logo-wrap">
+		<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
+		<p class="h1 logo"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+		<!-- if you'd like to use the site description you can un-comment it below -->
+		<p class="desc"><?php bloginfo('description'); ?></p>
+	</div>
+</div>
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,17 @@
-console.log('This would be the main JS file.');
+
+/**
+ * As the page loads, call these scripts
+ */
+jQuery(document).ready(function($) {
+
+	/* 
+	 * JQUERY MOBILE NAVIGATION
+	 * toggle sliding nav and show/hide class
+	 */
+	$(".menu-icon").on("click", function(){
+	    $(this).toggleClass('open-close');
+	    $("#mobile").slideToggle();
+	    $("#mobile").toggleClass('open-close');
+	}); 
+
+});
